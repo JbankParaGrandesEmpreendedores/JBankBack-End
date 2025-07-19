f9f26e17655216b0fe5776a32c9173f68628a9d6
Pedro Gabriel Scholemberg <pedrogabrielsc1812@gmail.com>
2025-07-19 14:40:28 -0300

    Repository created

---
+++ src/main/java/com/bank/jbank/repository/AgencyRepository.java
@@ -0,0 +1,10 @@
+package com.bank.jbank.repository;
+
+import com.bank.jbank.model.entity.Agency;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AgencyRepository extends JpaRepository<Integer, Agency> {
+
+}
