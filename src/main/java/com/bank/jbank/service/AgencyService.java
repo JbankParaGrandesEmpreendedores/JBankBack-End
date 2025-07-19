eed43f28d92a19e9760304c45f47270ef30d6f41
Pedro Gabriel Scholemberg <pedrogabrielsc1812@gmail.com>
2025-07-19 14:54:07 -0300

    Service file created

---
+++ src/main/java/com/bank/jbank/service/AgencyService.java
@@ -0,0 +1,9 @@
+package com.bank.jbank.service;
+
+import com.bank.jbank.model.entity.Agency;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgencyService {
+    public Agency
+}
