b01a52c07618501ff1b9512e562a6dbea42b2394
Pedro Gabriel Scholemberg <pedrogabrielsc1812@gmail.com>
2025-07-19 13:03:50 -0300

    Entity created

--- src/main/java/com/bank/jbank/model/entity/Agency.java
+++ src/main/java/com/bank/jbank/model/entity/Agency.java
@@ -1,4 +1,18 @@
 package com.bank.jbank.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Agency {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    String nome;
+    Endereco endereco;
 }
