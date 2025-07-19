7962726f7cf8924cb92b0b0f572398c04a160494
Pedro Gabriel Scholemberg <pedrogabrielsc1812@gmail.com>
2025-07-19 14:14:50 -0300

    Address entity created

--- src/main/java/com/bank/jbank/model/entity/Address.java
+++ src/main/java/com/bank/jbank/model/entity/Address.java
@@ -4,18 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Agency {
-
+public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
-    String nome;
-    Endereco endereco;
+    Long cep;
+    String estado;
+    String cidade;
+    String rua;
+    Integer numero;
 }
