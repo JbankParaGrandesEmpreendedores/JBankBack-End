3e5b9288ce3a43e05b2cc19a0445ca4291981238
Pedro Gabriel Scholemberg <pedrogabrielsc1812@gmail.com>
2025-07-19 14:28:55 -0300

    Association created

--- src/main/java/com/bank/jbank/model/entity/Agency.java
+++ src/main/java/com/bank/jbank/model/entity/Agency.java
@@ -1,9 +1,6 @@
 package com.bank.jbank.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,5 +14,7 @@ public class Agency {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
     String name;
+    @ManyToOne
+    @JoinColumn(name = "address_id")
     Address address;
 }
