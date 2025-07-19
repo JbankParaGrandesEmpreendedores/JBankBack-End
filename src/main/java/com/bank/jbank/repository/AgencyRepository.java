a9822d5ede6b41b79e8669a66038f7be87e44273
Pedro Gabriel Scholemberg <pedrogabrielsc1812@gmail.com>
2025-07-19 14:41:08 -0300

    JpaRepository key and value updated

--- src/main/java/com/bank/jbank/repository/AgencyRepository.java
+++ src/main/java/com/bank/jbank/repository/AgencyRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AgencyRepository extends JpaRepository<Integer, Agency> {
+public interface AgencyRepository extends JpaRepository<Agency, Integer> {
 
 }
