ed37cbc1a46956eee904c9de4220cee1b8ff3de9
Pedro Gabriel Scholemberg <pedrogabrielsc1812@gmail.com>
2025-07-21 18:00:32 -0300

    Encapsulation added

--- src/main/java/com/bank/jbank/model/entity/Agency.java
+++ src/main/java/com/bank/jbank/model/entity/Agency.java
@@ -12,9 +12,9 @@ public class Agency {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Integer id;
-    String name;
+    private Integer id;
+    private String name;
     @ManyToOne
     @JoinColumn(name = "address_id")
-    Address address;
+    private Address address;
 }
