8aa97668977b660199f2e14755709cf25d503dba
bino1432 <bernardozplz@gmail.com>
2025-07-28 18:17:14 -0300

    Its Running now

--- src/main/java/com/bank/jbank/model/entity/Agency.java
+++ src/main/java/com/bank/jbank/model/entity/Agency.java
@@ -14,6 +14,7 @@ public class Agency {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+    private Integer number;
     @ManyToOne(cascade = CascadeType.REMOVE)
     @JoinColumn(name = "address_id")
     private Address address;
